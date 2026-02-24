6a67c17e6cf528fe918ce0a5e66c2f6b5078f694
sho <yamasina0419@gmail.com>
2026-02-24 16:46:18 +0900

    Initial Commit

---
+++ movingTests/movingTests.swift
@@ -0,0 +1,17 @@
+//
+//  movingTests.swift
+//  movingTests
+//
+//  Created by Sho Yamasina on 2026/02/24.
+//
+
+import Testing
+@testable import moving
+
+struct movingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
